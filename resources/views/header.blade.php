62f6be063b43fb01c3a048f1847bf4a64de2bf33
Kavan Prajapati <kavan.prajapati@rayoinnovations.com>
2020-12-11 15:09:26 +0530

    login done with auth

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -24,7 +24,7 @@
                 <button type="submit" class="btn btn-default">Submit</button>
             </form>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Add to Cart</a></li>
+                <li><a href="#">Cart(0)</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
